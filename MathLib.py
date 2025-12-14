59ad511de68cc9788917d76b851fe6a41797f356
Guest-11111111 <nanda.swagatika@gmail.com>
2025-12-14 11:27:42 -0600

    Update MathLib.py

--- MathLib.py
+++ MathLib.py
@@ -311,85 +311,8 @@ def factorial(n:int):
     else:
         return n * factorial(n - 1)
 
-#constants
-def pi():
-    pi = 
-    return pi
-def phi():
-    phi = 1.618033988749895
-    return phi
-def e():
-    e = 2.71828182845904523536028747135266249775724709369995
-    return e
-def tau():
-    tau = 2*pi()
-    return tau
-def PythConst():
-    PC = sqrt(2)
-    return PC
-def TheoConst():
-    TC = sqrt(3)
-    return TC
-def SilvRatio():
-    SR = sqrt(2)+1
-    return SR
-def SuperGoldRatio():
-    SGR = ((1+cbrt((29+(3*sqrt(93)))/2)+cbrt((29-(3*sqrt(93)))/2))/3)
-    return SGR
-def i():
-    i = sqrt(-1)
-    return i
-def ConnConst():
-    CC = sqrt(2+sqrt(2))
-    return CC
-def GoldenAngleRad():
-    GAR = ((2*pi())/((phi())**2))
-    GARConf = (pi()*(3-sqrt(5)))
-    if GAR !=GARConf:
-        raise ValueError("SYSTEM ERROR")
-    else:
-        return GAR
-def GoldenAngleDeg():
-    GAD = 180*(3-sqrt(5))
-    GADConf1 = (180/pi())*((2*pi())/((phi())**2))
-    GADConf2 = (180/pi())*(pi()*(3-sqrt(5)))
-    if GAD != GADConf1 or GAD != GADConf2 or GADConf1 != GADConf2:
-        raise ValueError("SYSTEM ERROR")
-    else:
-        return GAD
-def GoldenAngleGrad():
-    GAG = (180*(3-sqrt(5)))*(180/200)
-    GAGConf =  ((2*pi())/((phi())**2))*(pi()/200)
-    if GAG == GAGConf:
-        return GAG
-    else: 
-        raise "SYSTEM ERROR: UNIDENTIFIED ERROR"
-def RamanujConst():
-    RamConst = (e()**(pi()*sqrt(163)))
-    return RamConst
-def UnivParaConst():
-    UPC = ln(1+sqrt(2))+sqrt(2) 
-    UPCConf= asinh(1)+sqrt(2)
-    if UPC != UPCConf:
-        raise ValueError("SYSTEM ERROR")
-    else:
-        AnswerSet =  UPC,UPCConf
-    return AnswerSet
-def GelfSchneidConst():
-    GSC = (2**sqrt(2))
-    return GSC
-def GelfConst():
-    GC = e()**pi()
-    return GC
-def FavardConst2():
-    SFC = (pi()**2)/8
-    return SFC
-def  LochConst():
-    LC = ((6*m.ln(2*m.ln(10)))/(pi()**2))
-    return LC
 
-
-# Trauma for the survivors:
+# Calculus by ai
 def Powinteg(x: dcml, n: dcml): # n = exponent
     IntegX = ((x**(n+1))/(n+1))
     return IntegX
@@ -477,6 +400,7 @@ class solg():
         CoVo = ((1/3)*(pi*(r**2)))
 
         return CoVo
+
 
 
 
